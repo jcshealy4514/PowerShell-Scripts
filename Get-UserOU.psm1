c6caedfef962dd73f37bc73a505f5b24b92137c6
jcshealy4514 <65636255+jcshealy4514@users.noreply.github.com>
2021-05-25 21:00:03 -0400

    Add files via upload

---
+++ Get-UserOU.psm1
@@ -0,0 +1,49 @@
+﻿Function Get-UserOU{
+     Param(
+	[Parameter(Mandatory,Position=0)]
+	[String]
+    $SamAccountName,
+
+    [Parameter(Mandatory=$False,Position=1)]
+    [String]
+    $Server,
+
+    [Parameter(Mandatory=$False,Position=2)]
+    [Switch]
+    $Hierarchy
+    )
+
+    if(!$Server){
+        $Server = $env:LOGONSERVER.Remove(0,2)
+    }
+
+    Try{
+        $UserData = Get-aduser $SamAccountName -server $Server
+    }
+    Catch{
+        Return write-host "`nUnable to find username: $samaccountname`nPlease try another username..." -ForegroundColor Red
+    }
+
+    $IndexOfString = $UserData.distinguishedname.indexof(",OU") + 1
+    $DNLength = $UserData.Distinguishedname.length - $IndexOfString
+    $UserOU = $UserData.DistinguishedName.Substring($IndexOfString,$DNLength)
+    if($Hierarchy){
+        $HierarchyOUs = @()
+        $HierarchyOUs += $UserOU
+        $CurrentDepth = $UserOU
+        for($i = 0;$i -lt 50; $i++){
+            $IndexOfString = $CurrentDepth.indexof(",OU") + 1
+            $DNLength = $CurrentDepth.length - $IndexOfString
+            $ParentOU = $CurrentDepth.Substring($IndexOfString,$DNLength)
+            $CurrentDepth = $ParentOU
+            $HierarchyOUs += $ParentOU
+            if($HierarchyOUs[-1] -eq $HierarchyOUs[-2]){
+                $HierarchyOUs[-1] = $null
+                return $HierarchyOUs = $HierarchyOUs | out-gridview -PassThru -Title "Select the OU('s) you wish to target."
+            }
+        }
+    }
+    else{
+        return $UserOU
+    }
+}
